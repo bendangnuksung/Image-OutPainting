be555ff94382bdafae42b99cad903df61217a411
bendangnuksung <bendangnuksung@gmail.com>
2018-07-20 22:53:36 +0530

    first commit

---
+++ dataloader.py
@@ -0,0 +1,44 @@
+import numpy as np
+import os
+from random import shuffle
+
+
+DATA_PATH = "data/prepared_data"
+
+
+class Data():
+
+	def __init__(self):
+		self.X_counter = 0
+		self.file_counter = 0
+		self.files = os.listdir(DATA_PATH)
+		shuffle(self.files)
+		self._load_data()
+
+	def _load_data(self):
+		datas = np.load(os.path.join(DATA_PATH, self.files[self.file_counter]))
+		self.X = []
+		for data in datas:
+			self.X.append(data)
+		self.X = np.asarray(self.X)
+		self.file_counter += 1
+
+
+	def get_data(self, batch_size):
+		if self.X_counter >= len(self.X):
+			if self.file_counter > len(self.files) - 1:
+				print("Data exhausted, Re Initialize")
+				self.__init__()
+				return None
+			else:
+				self._load_data()
+				self.X_counter = 0
+
+		if self.X_counter + batch_size <= len(self.X):
+			remaining = len(self.X) - (self.X_counter)
+			X = self.X[self.X_counter: self.X_counter + batch_size]
+		else:
+			X = self.X[self.X_counter: ]
+
+		self.X_counter += batch_size
+		return X
---
+++ prepare_data.py
@@ -0,0 +1,45 @@
+import numpy as np
+import os
+import cv2
+
+# raw_data_path: directory where the downloaded images are
+# save_path: directory where the numpy images will be
+raw_data_path = "data/raw_data/cars_train"
+save_path = "data/prepared_data/"
+
+files = os.listdir(raw_data_path)
+input_shape = (256, 256)
+
+# batch: each file will have N images
+batch = 1000
+
+
+# Dumping numpy batch images to save_path
+counter = 1
+def dumpy_numpy(data):
+	global counter
+	file_path = os.path.join(save_path, str(counter))
+	np.save(file_path, data)
+	counter += 1
+
+
+# Converting to numpy files
+bulk = []
+if not len(files):
+	print("No images in: ", raw_data_path)
+for i, file in enumerate(files, 1):
+	try:
+		image_path = os.path.join(raw_data_path, file)
+		image = cv2.imread(image_path)
+		image = cv2.resize(image, input_shape)
+		bulk.append(image)
+	except Exception as e:
+		print("error: ", e)
+		print("file name: ", image_path)
+
+	if len(bulk) >= batch or i == len(files):
+		print("Dumping batch: ", batch)
+		dumpy_numpy(bulk)
+		bulk = []
+
+	print("Proccessed: %s / %s image" %(i, len(files)))
